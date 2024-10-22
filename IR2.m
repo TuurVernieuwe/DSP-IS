92bad3cb45f651eb23199d1c1201de70e85e7b74
Tuur Vernieuwe <tuur.vernieuwe@student.kuleuven.be>
2024-10-22 10:34:22 +0200

    added white noise experiment

--- IR2.m
+++ IR2.m
@@ -50,38 +50,42 @@ plot(pow2db(abs(fft(h, dftsize))));
 xlabel('Frequency [Hz]')
 ylabel('Magnitude response [dB]')
 
-% %% Filtered white noise Vs. recorded white noise.
-% sig = wgn(); % Generate white noise signal    
-% 
-% % Convolve the IR with the white noise
-% gen_noise = fftfilt();                  
-% 
-% % Send the white noise across the channel
-% [] = initparams();
-% sim('recplay');
-% rec_noise=simout.signals.values(:,1);
-% 
-% %% Spectrogram and PSD plot
-% % Spectrogram
-% spectrogram(); % Generated white noise
-% spectrogram(); % Recorded white noise
-% 
-% % PSD
-% [] = spectrogram(); % Generated white noise
-% [] = spectrogram(); % Recorded white noise
-% 
-% PSD_gen_noise = ; % Generated white noise PSD
-% PSD_rec_noise = ; % Recorded white noise PSD (ideally only when the signal is active)
-% 
-% figure;
-% plot(,pow2db());
-% xlabel('Frequency (kHz)');
-% ylabel('Power/frequency (dB/Hz)')
-% title('Generated white noise.')
-% % Output signal
-% subplot(2,2)
-% plot(,pow2db());
-% xlabel('Frequency (kHz)');
-% ylabel('Power/frequency (dB/Hz)')
-% title('Output signal.')
-% sgtitle('Recorded white noise')
+%% Filtered white noise Vs. recorded white noise.
+sig = wgn(duration*fs, 1, 1); % Generate white noise signal    
+
+% Convolve the IR with the white noise
+gen_noise = fftfilt(h, sig);                  
+
+% Send the white noise across the channel
+[simin, nbsecs, fs] = initparams(sig, fs);
+sim('recplay');
+rec_noise=simout.signals.values(:,1);
+
+%% Spectrogram and PSD plot
+% Spectrogram
+figure;
+subplot(2, 1, 1);
+spectrogram(gen_noise); % Generated white noise
+subplot(2, 1, 2);
+spectrogram(rec_noise); % Recorded white noise
+
+% PSD
+[S_gen, F_gen, T_gen, P_gen] = spectrogram(gen_noise); % Generated white noise
+[S_rec, F_rec, T_rec, P_rec] = spectrogram(rec_noise); % Recorded white noise
+
+PSD_gen_noise = mean(P_gen, 2); % Generated white noise PSD
+PSD_rec_noise = mean(P_rec, 2); % Recorded white noise PSD (ideally only when the signal is active)
+
+figure;
+subplot(2, 1, 1);
+plot(F_gen, pow2db(PSD_gen_noise));
+xlabel('Frequency (kHz)');
+ylabel('Power/frequency (dB/Hz)')
+title('Generated white noise.')
+% Output signal
+subplot(2, 1, 2);
+plot(F_rec ,pow2db(PSD_rec_noise));
+xlabel('Frequency (kHz)');
+ylabel('Power/frequency (dB/Hz)')
+title('Output signal.')
+sgtitle('Recorded white noise')
